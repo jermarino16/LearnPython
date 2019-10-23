dd42a2cc1175fdd8b381d900caaf31b523fb2542
Jeremy Marino <jermarino16@gmail.com>
2019-10-23 08:13:41 -0700

    Added Escape Sequences files

---
+++ Variables/EscapeSequences/escapeSequences.py
@@ -0,0 +1,13 @@
+ # Set the message variable equal to any string containing a new-line escape sequence
+message = "hello \n cutie"
+print(message)
+
+# Add a string to the mountains variable that when printed results in: /\/\/\
+# You will need to use an escape sequence more than once!
+mountains = "/\\/\\/\\"
+print(mountains)
+
+# Set the quotation variable to any string that contains an escaped double quotation mark
+
+quotation = "you are \"cool\""
+print(quotation)
