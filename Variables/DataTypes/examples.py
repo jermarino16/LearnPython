da450878153acad415e91a67d4b0cc137264a8d7
Jeremy Marino <jermarino16@gmail.com>
2019-10-23 07:52:39 -0700

    added data type examples

---
+++ Variables/DataTypes/examples.py
@@ -0,0 +1,18 @@
+#bool
+jeremy_test = True
+print(jeremy_test)
+
+jeremy_bad_test = False
+print(jeremy_bad_test)
+
+#strings
+some_string = "10"
+print(some_string)
+
+
+
+
+#theoretical ouput
+#True
+#False
+#10
