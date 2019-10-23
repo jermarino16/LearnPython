0f90edd60b4b35126bcde7295dfae345b479aff4
Jeremy Marino <jermarino16@gmail.com>
2019-10-23 07:33:37 -0700

    Added Variables folder for Python learning

---
+++ Variables/comments.py
@@ -0,0 +1,8 @@
+# I already know what variables are, learning how python defines variables
+num_of_dogs = 99
+
+print(num_of_dogs) #should print 99
+
+num_of_dogs = num_of_dogs - 9 
+
+print(num_of_dogs) #should print 90
