dcb9b176abdde97db509471291cc4e7fb186c8b6
Jeremy Marino <jermarino16@gmail.com>
2019-10-23 07:55:49 -0700

    added message on dynamic typing

---
+++ Variables/DataTypes/dynamicTyping.py
@@ -0,0 +1,15 @@
+#python is dynamically typed, we can change variables at any time
+
+jeremy = True
+print(jeremy)
+
+jeremy = 100
+print(jeremy)
+
+jeremy = "jeremy is cool"
+print(jeremy)
+
+#output should be
+#True
+#100
+#jeremy is cool
