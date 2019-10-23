5c68875a8456fb987e3b5476dca285756dbafccb
Jeremy Marino <jermarino16@gmail.com>
2019-10-23 07:26:45 -0700

    Added comments to this file

--- NumbersAndMath/comments.py
+++ NumbersAndMath/comments.py
@@ -1,3 +1,7 @@
 print (1/3) #division returns float, even when dividing two ints
 
 ## comment here easy pz
+
+## Taking "The Modern Python 3" Bootcamp
+
+## Section was pretty easy just exposes us to all of the different ways python3 handles number operations, pretty straightforward
