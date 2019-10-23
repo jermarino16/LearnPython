e71fe5eea731c90cdc223bfbf9c2efe40f6b20fc
Jeremy Marino <jermarino16@gmail.com>
2019-10-23 07:44:04 -0700

    added naming conventions file

---
+++ Variables/naming_conventions.py
@@ -0,0 +1,6 @@
+#variables should be underscores between words, snake_case
+#variables should be lowercase
+#all upper case should be constants
+#classes should be like this UpperCamelCase
+#variables that start and end with two underscores are supposed to be private or left alone
+# ex: __no_touchy__
