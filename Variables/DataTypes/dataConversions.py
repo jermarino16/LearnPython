ed9655605bb149598cc59314a1721873f47bd830
Jeremy Marino <jermarino16@gmail.com>
2019-10-24 07:54:45 -0700

    added some comments to data conversion file

--- Variables/DataTypes/dataConversions.py
+++ Variables/DataTypes/dataConversions.py
@@ -1,16 +1,15 @@
 #we can explicitily convert variables by using the name of the builtin type as a function
-decimal = 12.523523
+#floats can be turned into integers, (rounds down)
+decimal = 12.923523
 number = int(decimal)
-
 print(number)
 
+#integers can be turned to flots
 num = 15
-
 floatyguy = float(num)
-
 print(floatyguy)
 
+#strings can be converted to numbers
 stringNumber = "8"
-
 realNumber = int(stringNumber)
 print(realNumber)
