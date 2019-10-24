5f7d47d2cffc1fe3ed721fc3805d967a1517e138
Jeremy Marino <jermarino16@gmail.com>
2019-10-24 07:52:56 -0700

    added data conversions file

---
+++ Variables/DataTypes/dataConversions.py
@@ -0,0 +1,16 @@
+#we can explicitily convert variables by using the name of the builtin type as a function
+decimal = 12.523523
+number = int(decimal)
+
+print(number)
+
+num = 15
+
+floatyguy = float(num)
+
+print(floatyguy)
+
+stringNumber = "8"
+
+realNumber = int(stringNumber)
+print(realNumber)
