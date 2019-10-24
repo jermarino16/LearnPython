a0dc4e0d1ebee812f2991e10de648e66d5a45da1
Jeremy Marino <jermarino16@gmail.com>
2019-10-24 07:41:19 -0700

    added string indeces to strings file

--- Variables/DataTypes/strings.py
+++ Variables/DataTypes/strings.py
@@ -23,3 +23,10 @@ print(string4)
 ## we can use f-strings, formatted strings to print stuff as well
 x = 10
 print(f"The number is {x}")
+
+## we can access string indeces like other programming lanagues 
+answer = "yes"
+
+print(answer)
+print(answer[0])
+print(answer[-2])
