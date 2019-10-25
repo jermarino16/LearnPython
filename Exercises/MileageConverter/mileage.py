159fedf53141b5beab7bb79d3054be730721075d
Jeremy Marino <jermarino16@gmail.com>
2019-10-24 17:14:44 -0700

    changed syntax on mileage exercise

--- Exercises/MileageConverter/mileage.py
+++ Exercises/MileageConverter/mileage.py
@@ -2,4 +2,4 @@ print("How many kilometers did you cycle today?")
 kms = input()
 miles = float(kms)/1.60394
 miles = round(miles, 2)
-print(f"That is equal to {miles} miles")
+print(f"Your {kms}km ride was {miles} miles")
