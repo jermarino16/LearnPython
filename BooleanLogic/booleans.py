b8917a00200cf8035d45f7590a10d47fa8b93cb4
Jeremy Marino <jermarino16@gmail.com>
2019-10-24 17:30:59 -0700

    added a boolean logic file

---
+++ BooleanLogic/booleans.py
@@ -0,0 +1,13 @@
+print("input your name")
+name = input()
+
+#boolean logic, if, elif, or else. indentation matters
+if name == "Jeremy":
+	print("You are cute")
+elif name == "Bob":
+	print("Weirdo")
+elif name == "Erica":
+	print("\nDang you are the cutest")
+	print("Like the real cutest")
+else:
+	print("Get outta here")
