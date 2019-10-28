9305b07fb1328e9b6fe8e7ba6764b44cad28a915
Jeremy Marino <jermarino16@gmail.com>
2019-10-28 16:53:14 -0700

    did another loop example

--- Loops/loops.py
+++ Loops/loops.py
@@ -7,3 +7,11 @@ print("")
 #prints every number in the range
 for x in range (1, 10):
 	print(x)
+
+#print out the sum of the odd numbers in the range 10 to 20
+x = 0
+
+for y in range (11, 21, 2):
+	x += y
+
+print(f"The sum of the odd numbers between 10 and 20 is {x}")
