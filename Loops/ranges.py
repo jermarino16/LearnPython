6d35175391420add2148ba4dfd8ff2facac7bbeb
Jeremy Marino <jermarino16@gmail.com>
2019-10-28 16:45:53 -0700

    exercise on ranges for understanding

---
+++ Loops/ranges.py
@@ -0,0 +1,19 @@
+#regular range(num) example
+print("we are going to do a range(7) example")
+for x in range(7):
+	print(x)
+
+#interval range example
+print("we are going to do a range(2, 8) example")
+for x in range(2, 8):
+	print(x)
+
+#interval with step range example
+print("we are going to do a range(1, 10, 2) example")
+for x in range(1, 10, 2):
+	print(x)
+
+#interval but counting down example
+print("we are going to do a range(7, 0, -1) example")
+for x in range (7, 0, -1):
+	print(x)
