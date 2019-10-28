5de9bd4b190153a3a96b618478d7a67d29754c25
Jeremy Marino <jermarino16@gmail.com>
2019-10-27 22:27:54 -0700

    did a bouncer exercise to simulate a bouncer

---
+++ Exercises/Bouncer/bouncer.py
@@ -0,0 +1,15 @@
+#this program will simulate a bouncer
+
+#ask for age
+print("What is your name")
+age = input()
+
+#check for 21 +
+if (age >= 21):
+	print("Come in and party")
+#check for 18 - 21
+elif (age >= 18 and age < 21):
+	print("Come in, but no drinks for you")
+#check for < 18
+else
+	print("You can't come in baby")
