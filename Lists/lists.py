21364e740f6a9a0557945ee9a79c846bb8c1fa4e
Jeremy Marino <jermarino16@gmail.com>
2019-10-29 06:24:18 -0700

    small list example

---
+++ Lists/lists.py
@@ -0,0 +1,10 @@
+
+# Define my_stuff 
+my_stuff = [1, 2, True, 4, 5, "hey", 3.0]
+print("Lists can contain many different types")
+print(my_stuff)
+
+
+nums = list((range(1, 100))
+print("\nLists can be easily made with range function")
+print(nums)
