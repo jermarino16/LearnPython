d52b781bd3bf929b2e5668238fd30e3a2639f77c
Jeremy Marino <jermarino16@gmail.com>
2019-10-29 07:27:15 -0700

    exercise on slicing lists

---
+++ Exercises/ListMethods/slices.py
@@ -0,0 +1,16 @@
+my_list = list(range(1, 10))
+
+#slice starting at the 1st index
+print(my_list[1:])
+
+#slice at the 5th index
+print(my_list[5:])
+
+#slice the whole thing? :O
+print(my_list[:])
+
+#slice from 4 to 8
+print(my_list[4:8])
+
+#slice it all with a 2 step
+print(my_list[0:9:2])
