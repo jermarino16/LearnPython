f672b551b012c3f657be2d5968f34efb119c839e
Jeremy Marino <jermarino16@gmail.com>
2019-10-29 06:28:57 -0700

    added an exercise on accessing data

--- Lists/lists.py
+++ Lists/lists.py
@@ -8,3 +8,16 @@ print(my_stuff)
 nums = list((range(1, 100))
 print("\nLists can be easily made with range function")
 print(nums)
+
+
+#accessing list data
+people = ["Hanna","Louisa","Claudia", "Angela","Geoffrey", "aparna"]
+
+#change Hanna to Hannah
+people[0] = "Hannah"
+
+#change Geoffrey to Jeffrey
+people[4] = "Jeffrey"
+
+#change aparna to Aparna
+people[5] = "Aparna"
