7b65a494407949dca0d241f18d84c2150d3a8723
Jeremy Marino <jermarino16@gmail.com>
2019-10-28 18:51:56 -0700

    small exercise on loops and for statements, clean up your room

---
+++ Exercises/ScreamingRepeating/clearnYourRoom.py
@@ -0,0 +1,12 @@
+print("How many times do I have to tell you?")
+
+tell_me_this_many_times = input()
+
+if tell_me_this_many_times:
+	tell_me_this_many_times = int(tell_me_this_many_times)
+
+	for time in range(tell_me_this_many_times):
+		print("CLEAN UP YOUR ROOM! CHILD!")
+
+else:
+	print("Enter something yo")
