d1e0a405bf11f79b4331a7a4fa200aa3eb8a7ca8
Jeremy Marino <jermarino16@gmail.com>
2019-10-29 06:36:46 -0700

    did a for loop and while loop example for lists

---
+++ Lists/loopLists.py
@@ -0,0 +1,16 @@
+colors_and_more = ["red", "purple", "blue", True, 5.0]
+
+#for loop exammple, just prints the colors
+print("\nFor loop example")
+for data in colors_and_more:
+	print(data)
+
+print("The List is: ")
+print(colors_and_more)
+
+#while loop example
+i = 0
+print ("\nWhile loop example")
+while i < len(colors_and_more):
+	print(colors_and_more[i])
+	i += 1
