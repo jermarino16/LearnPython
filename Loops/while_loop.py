af4d694d1ff75bb927e3ff12baebbfda84b92ac8
Jeremy Marino <jermarino16@gmail.com>
2019-10-28 19:04:32 -0700

    small exercise on while loops

---
+++ Loops/while_loop.py
@@ -0,0 +1,8 @@
+msg = input("What's the secret password? ")
+
+#loop until they say im awesome
+while msg != "jeremy is awesome":
+	print("Wrong try again")
+	msg = input("What's the secret password? ")
+
+print("nah you are cutie ;)")
