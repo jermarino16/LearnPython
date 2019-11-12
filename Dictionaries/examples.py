6a0bc69bc5f38e7f5066a743098194950e3faa3b
Jeremy Marino <jermarino16@gmail.com>
2019-11-11 21:36:18 -0800

    simple dictionary examples

---
+++ Dictionaries/examples.py
@@ -0,0 +1,5 @@
+dictionary_test = {"cat": "cool", "dog": "cooler", "age": 24, "isAwesome": True }
+print(f"The dictionary looks like: {dictionary_test}")
+
+dictionary_test2 = dict(name = "jeremy", age=50)
+print(f"The 2nd dictionary looks like: {dictionary_test2}")
