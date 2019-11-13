61c0d22195e36ec7d7e16da0a5187ad67fbef477
Jeremy Marino <jermarino16@gmail.com>
2019-11-12 17:40:53 -0800

    did example of iterating over dictionaries

--- Dictionaries/examples.py
+++ Dictionaries/examples.py
@@ -8,3 +8,18 @@ print(f"The 2nd dictionary looks like: {dictionary_test2}")
 #access dicitonary
 name_Dictionary = dictionary_test2["name"]
 print(f"The name of dictionary_test2 is: {name_Dictionary}")
+
+#iterate over a dictionary
+instructor = {"name": "Jeremy", "age": 25, "isCool": True}
+
+print ("\nThe values of instrcutor are: ")
+for value in instructor.values():
+	print(value)
+
+print ("\nThe keys of instrcutor are: ")
+for keys in instructor.keys():
+	print(keys)
+
+print ("\nThe items of instrcutor are: ")
+for key, value in instructor.items():
+	print(f"The key is {key}, The value is {value})
