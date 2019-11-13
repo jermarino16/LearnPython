49a744c270e8af435b3fc3923bb9f7cb21455436
Jeremy Marino <jermarino16@gmail.com>
2019-11-12 17:34:05 -0800

    did example of accessing dictionaries

--- Dictionaries/examples.py
+++ Dictionaries/examples.py
@@ -1,5 +1,10 @@
+#create dictionary
 dictionary_test = {"cat": "cool", "dog": "cooler", "age": 24, "isAwesome": True }
 print(f"The dictionary looks like: {dictionary_test}")
 
 dictionary_test2 = dict(name = "jeremy", age=50)
 print(f"The 2nd dictionary looks like: {dictionary_test2}")
+
+#access dicitonary
+name_Dictionary = dictionary_test2["name"]
+print(f"The name of dictionary_test2 is: {name_Dictionary}")
