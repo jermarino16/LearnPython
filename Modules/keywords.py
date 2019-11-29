184303298d96cd11ae8cd5562ec96df1adac1e0e
Jeremy Marino <jermarino16@gmail.com>
2019-11-29 08:52:47 -0800

    fixed bug where there was a circular dependency, did a module example

---
+++ Modules/keywords.py
@@ -0,0 +1,9 @@
+import keyword
+
+#check to see if any strings are keywords, otherwise returns false
+def contains_keyword(*args):
+    for arg in args:
+        if keyword.iskeyword(arg):
+            return True
+
+    return False
