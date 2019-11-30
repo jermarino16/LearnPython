3f332fe86d0da54981979c74fb348035ec5772cb
Jeremy Marino <jermarino16@gmail.com>
2019-11-30 07:41:22 -0800

    function that makes art of text and adds color

--- Modules/keywords.py
+++ Modules/keywords.py
@@ -1,6 +1,8 @@
 import keyword
 
-#check to see if any strings are keywords, otherwise returns false
+# check to see if any strings are keywords, otherwise returns false
+
+
 def contains_keyword(*args):
     for arg in args:
         if keyword.iskeyword(arg):
