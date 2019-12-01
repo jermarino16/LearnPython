a6d4fbc1e8a4c02ebc53b101b8d7c22b872171fb
Jeremy Marino <jermarino16@gmail.com>
2019-11-30 17:11:35 -0800

    made a random dad joke api request from website

--- HTTP/dad_joke.py
+++ HTTP/dad_joke.py
@@ -1,8 +1,21 @@
 import requests
-url = "https://icanhazdadjoke.com/"
+import random
 
-response = requests.get(url, headers = {"Accept": "application/json"})
+url = "https://icanhazdadjoke.com/search"
+
+user_input = input("What type of dad jokes do you want? \n")
+
+response = requests.get(
+	url, 
+	headers = {"Accept": "application/json"}, 
+	params = { "term": user_input } )
 
 data = response.json()
 
-print(data["joke"])
+if(data["results"]):
+	num_of_jokes = len(data["results"])
+	random_number = random.randint(0, num_of_jokes - 1)
+
+	print(f"There was {num_of_jokes} jokes: Here's one: \n {data['results'][random_number]['joke']}")
+else:
+	print(f"there are no jokes about {user_input}")
