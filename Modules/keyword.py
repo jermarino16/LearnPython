6ce1452708aff944607a7346d6116012c0c1b77d
Jeremy Marino <jermarino16@gmail.com>
2019-11-30 17:17:18 -0800

    removed keyword file

--- Modules/keyword.py
+++
@@ -1,8 +0,0 @@
-import keyword
-#check to see if any strings are keywords, otherwise returns false
-def contains_keyword(*args):
-    for arg in args:
-        if keyword.iskeyword(arg):
-            return True
-
-    return False
