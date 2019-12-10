4eeb09d5da60581f82bd44701d09f7debc4fed2b
Jeremy Marino <jermarino16@gmail.com>
2019-12-10 06:27:48 -0800

    created the init function

--- Classes/first_class.py
+++ Classes/first_class.py
@@ -4,7 +4,13 @@
 
 # instantiate a new Vehicle and save it to a variable called boat:
 class Vehicle:
-    pass
+    def __init__(self, make, model, year):
+    	self.make = make
+    	self.model = model
+    	self.year = year
 
-car = Vehicle()
-boat = Vehicle()
+car = Vehicle("ford", "fiesta", 2099)
+boat = Vehicle("big", "boat", 2011)
+
+print(car.make, car.model)
+print(boat.make, boat.model)
