3a676a6a419c3ef7e267d0f1b8824eb74075a250
Jeremy Marino <jermarino16@gmail.com>
2019-12-10 06:22:49 -0800

    created a dummy class

---
+++ Classes/first_class.py
@@ -0,0 +1,10 @@
+# define the Vehicle class below:
+
+# instantiate a new Vehicle and save it to a variable called car:
+
+# instantiate a new Vehicle and save it to a variable called boat:
+class Vehicle:
+    pass
+
+car = Vehicle()
+boat = Vehicle()
