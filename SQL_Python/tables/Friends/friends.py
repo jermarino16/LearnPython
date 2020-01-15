025aaf515885acbb7897eb2203990ac470ea39d4
Jeremy Marino <jermarino16@gmail.com>
2020-01-15 10:20:29 -0800

    added how to bulk insert into sql table

--- SQL_Python/tables/Friends/friends.py
+++ SQL_Python/tables/Friends/friends.py
@@ -11,13 +11,26 @@ c = conn.cursor()
 insert_query = '''INSERT INTO friends
 					VALUES ('Merriwehter', 'Lewis', 7)'''
 
-form_first = "mary-todd"
-query = f"INSERT INTO friends (first_name) VALUES (?)"
-c.execute(query, (form_first,))
+# form_first = "mary-todd"
+# query = f"INSERT INTO friends (first_name) VALUES (?)"
+# c.execute(query, (form_first,))
 
 # data = ("steve", "irwin", 9)
 # query = "INSERT INTO friends VALUES (?,?,?)"
 # c.execute(query, data)
+
+#do many functions
+people = [
+	("Jeremy", "Marino", 5),
+	("Bubs", "Marino", 5),
+	("Erica", "Marino", 5)
+]
+
+# for person in people: # one way to insert a lot
+# 	c.execute("INSERT INTO friends VALUES (?,?,?)", person)
+# 	print("Inserted yo")
+
+# c.executemany("INSERT INTO friends VALUES(?,?,?)", people) # another way
 
 #commit changes
 conn.commit()
