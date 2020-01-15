34c4c6c63a9d0d8e825508cde8d681af61a53b91
Jeremy Marino <jermarino16@gmail.com>
2020-01-15 10:02:19 -0800

    sql python practice

---
+++ SQL_Python/tables/basics.sql
@@ -0,0 +1,4 @@
+INSERT INTO dogs(name, age, breed) VALUES("Champ", "Retriever", 4);
+INSERT INTO dogs(name, age, breed) VALUES("Bro", "Lab", 3);
+INSERT INTO dogs(name, age, breed) VALUES("CBoss", "Pitbull", 1);
+INSERT INTO dogs(name, age, breed) VALUES("Rosie", "German", 14);
